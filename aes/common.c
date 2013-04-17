4d603621778576f18e9a3949012830d102d40ee2
james <james@hypernova.(none)>
2013-04-17 00:08:59 -0400

    #defines in aes.h, made (empty) function declarations for encryption functions.  Goats are like mushrooms. If you shoot a duck, I'm scared of toasters.

---
+++ aes/common.c
@@ -0,0 +1,21 @@
+#ifndef COMMON_C
+#define COMMON_C
+
+#include "aes.h"
+
+unsigned int SubBytes(unsigned int key, unsigned int[STATE_ROWS][STATE_COLUMNS] state)
+{
+    
+}
+
+unsigned int ShiftRows(unsigned int key, unsigned int[STATE_ROWS][STATE_COLUMNS])
+{
+
+}
+
+unsigned int MixColumns(unsigned int key, unsigned int[STATE_ROWS][STATE_COLUMNS])
+{
+
+}
+
+#endif
