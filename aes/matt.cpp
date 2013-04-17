7c7a00b3295ee4d56132f01f188571fcf4b9d111
Matthew O'Neil <matt125@vt.edu>
2013-04-17 09:04:37 -0400

    Added shift bytes

---
+++ aes/matt.cpp
@@ -0,0 +1,24 @@
+void AESShiftRows16(unsigned char *bytePtr)
+{
+    // Each row needs to shift left by the corresponding values
+    int offsets[4] = {0, 1, 2, 3};
+    int temp[4];
+
+    // loops through rows. row 0 offset is 0 so skip it
+    for (int row = 1; row < 4; row++)
+    {
+	// Feel free to rewrite
+	// Finds the individual offset
+	int offset = offsets[row];
+
+	// Stores the values temporarily
+	for (int col = 0; col < 4; col++)
+	    temp[col] = bytePtr[row*4+col];
+
+	// Withdraws the values in the right locations
+	for (int col = 0; col < 4; col++)
+	    bytePtr[row*4 + col] = temp[(col+offset)%4];
+    }
+}
+
+
