57a775829fcba7c731873d63b03bdc9ef0cb7d2f
James Gross <fgjames9@gmail.com>
2013-04-16 21:31:51 -0400

    Function prototypes and a comment in aes.h. Just the like gypsy woman said

--- aes/aes.h
+++ aes/aes.h
@@ -1,6 +1,13 @@
 #ifndef AES_H
 #define AES_H
 
+unsigned int SubBytes(unsigned int key, unsigned int[4][6] state);//need state and key and what else?
 
+unsigned int ShiftRows(unsigned int key, unsigned int[4][6] state);
+
+unsigned int MixColumns(unsigned int key, unsigned int[4][6] state);
+
+//Generates round keys from the main key using Rijndael's key scheduler and the main key
+unsigned int AddRoundKeys(unsigned int key, unsigned int[4][6] state);
 
 #endif
